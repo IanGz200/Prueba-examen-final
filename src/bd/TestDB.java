c6c78d64a0e79e1076ed1c1189c069ea9d83c829
usuario <usuario@DAW1-42.dip.local>
2024-06-03 12:58:07 +0200

    a

--- src/bd/TestDB.java
+++ src/bd/TestDB.java
@@ -2,13 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package bd;
+
+import java.util.ArrayList;
+import modelo.Test;
 
 /**
  * -Comenta la clase-
+ *
  * @author IanGz
  */
 public class TestDB {
+
+    private static ArrayList<Test> tests = new ArrayList<>();
+
+    /**
+     *
+     * @return
+     */
+    public static ArrayList<Test> getTests() {
+        return tests;
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public static Test findById(int id) {
+
+        return null;
+
+    }
+
+    /**
+     *
+     * @param test
+     */
+    public static void saveTest(Test test) {
+
+        tests.add(tests.size(), test);
+        
+    }
 
 }
--- src/modelo/Test.java
+++ src/modelo/Test.java
@@ -2,53 +2,80 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package modelo;
 
 import java.util.ArrayList;
 
 /**
  * -Comenta la clase-
+ *
  * @author IanGz
  */
 public class Test {
 
     private int numero;
-    
+
     private String titulo;
-    
+
     private ArrayList<TestQuestion> preguntas;
 
+    /**
+     *
+     * @return
+     */
     public int getNumero() {
         return numero;
     }
 
+    /**
+     *
+     * @param numero
+     */
     public void setNumero(int numero) {
         this.numero = numero;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     *
+     * @param titulo
+     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<TestQuestion> getPreguntas() {
         return preguntas;
     }
 
+    /**
+     *
+     * @param preguntas
+     */
     public void setPreguntas(ArrayList<TestQuestion> preguntas) {
         this.preguntas = preguntas;
     }
 
+    /**
+     *
+     * @param numero
+     * @param titulo
+     */
     public Test(int numero, String titulo) {
         this.numero = numero;
         this.titulo = titulo;
-        this.preguntas =  new ArrayList<>();
+        this.preguntas = new ArrayList<>();
     }
-    
-    
-    
+
 }
